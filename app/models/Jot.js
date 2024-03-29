8399c8f50ac212f159d41f514b52efae71e9f0f3
Jarrett Cameron <native@hotmail.com>
2024-03-29 14:16:18 -0600

    Fixed edit & delete buttons stacking with name overflow on mobile

--- app/models/Jot.js
+++ app/models/Jot.js
@@ -9,16 +9,16 @@ export class Jot {
   }
 
   get selectMenuTemplate() {
-    return `<div class="py-2 col-12">
-        <div class="ps-4 row align-content-center justify-content-sm-around justify-content-end">
-          <div class="col">
+    return `<div class="py-sm-2 py-4 col-12">
+        <div class="ps-sm-4 row align-content-center justify-content-sm-around justify-content-center">
+          <div class="col-sm-6 col-12 text-sm-start text-center">
             <h4><span class="border-bottom border-5" style="border-color: ${this.color} !important;">${this.name}</span></h4>
           </div>
-          <div class="col-xl-2 col-sm-3 col-5 text-end">
-            <button class="btn fs-4 float-end" onclick="app.JotsController.selectActive('${this.name}')">EDIT</button>
+          <div class="col-xl-2 col-sm-3 col-6 pt-sm-0 pt-3">
+            <button class="btn d-sm-block d-inline text-sm-end text-center fs-4 float-sm-end w-100" onclick="app.JotsController.selectActive('${this.name}')">EDIT</button>
           </div>
-          <div class="col-xl-2 col-sm-3 col-5 text-end">
-            <button class="btn fs-4 text-danger float-end" onclick="app.JotsController.destroyJot('${this.name}')">DELETE</button>
+          <div class="col-xl-2 col-sm-3 col-6 pt-sm-0 pt-3">
+            <button class="btn d-sm-block d-inline text-sm-end text-center fs-4 text-danger float-sm-end w-100" onclick="app.JotsController.destroyJot('${this.name}')">DELETE</button>
           </div>
         </div>
       </div>`
